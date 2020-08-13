06badda422edad3dc95e7e4203c2a3062e272bf2
Yani Asari <yani.iman1@gmail.com>
2020-08-13 21:45:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,21 @@
-class Dog 
-  def initialize 
+class Dog
+  def initialize(name)
+    @name = name
+  end 
+  def name=(name)
+    @name = name
+  end 
+  def name
+    @name
+  end 
+  def initialize(name, breed)
+  if breed.none?
+    puts "#{name}, Mutt"
+  else 
+    def breed=(breed)
+    @breed = breed
+  end 
+  def breed
+    @breed
+  end 
+end 
