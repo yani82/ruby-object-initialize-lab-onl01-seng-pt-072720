0c1eba1a46444e9cefa08d364673b07091c6e00f
Yani Asari <yani.iman1@gmail.com>
2020-08-13 23:37:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,3 +4,21 @@ class Dog
     @breed = breed 
   end 
   
+  
+  def name=(name)
+    @name = name
+  end 
+  
+  def name
+    @name
+  end 
+  
+  def breed=(breed)
+  @breed = breed 
+  end 
+
+    def breed
+    @breed 
+  end 
+  
+end 
