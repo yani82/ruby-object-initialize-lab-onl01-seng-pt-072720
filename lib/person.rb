f86c745e7c93b1152404f3eb1e534d931ca20781
Yani Asari <yani.iman1@gmail.com>
2020-08-14 00:56:29 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person
+  
+  def initialize(name = "Beyonce")
+    @name = name 
+  end 
+  
+  def name=(name)
+    @name = name
+  end 
+  
+  def name
+    @name
+  end 
+  
+  def name=(name)
+    @name = name 
+  end 
+
+    def name
+    @name 
+  end 
+  
+end 
